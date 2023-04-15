b2cf73f0c317d19ed3789ed6159b90a58475c512
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-04-15 07:13:02 -0400

    grid is complete

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -30,118 +30,142 @@ const initialIndex = 4 // the index the "B" is at
 export default class AppClass extends React.Component {
   constructor(props) {
     super(props);
-   this.state = {
+    this.state = {
       message: initialMessage,
       email: initialEmail,
       index: initialIndex,
       steps: initialSteps,
       click: 4
     }
+
   }
 
-
   changingtheBLeft = (e) => {
-    dontmoveleft.includes(this.state.index) ? this.setState({ ...this.state }) : this.setState({ ...this.state, steps: this.state.steps + 1 , index: this.state.index - 1 });
-   
+    dontmoveleft.includes(this.state.index) ? this.setState({ ...this.state }) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index - 1 });
+    if (e.target.id === "left" && dontmoveleft.includes(this.state.index)) {
+      this.setState({ ...this.state, click: 0 })
+    }
+    else{
+      this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index - 1 , click: 4})
+    }
   }
 
   changingtheBright = (e) => {
-    dontmoveright.includes(this.state.index) ? this.setState({...this.state}) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index + 1 })
+    dontmoveright.includes(this.state.index) ? this.setState({ ...this.state }) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index + 1 })
+    if (e.target.id === "right" && dontmoveright.includes(this.state.index)) {
+      this.setState({ ...this.state, click: 3 })
+    }
+    else{
+      this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index + 1 , click: 4})
+    }
   }
 
   changingtheBup = (e) => {
-    this.state.index <= up ? this.setState({...this.state}) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index - 3 })
+    this.state.index <= up ? this.setState({ ...this.state }) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index - 3 })
+    if (e.target.id === "up" && this.state.index <= up) {
+      this.setState({ ...this.state, click: 1 })
+    }
+    else{
+      this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index - 3 , click: 4})
+    }
   }
 
-  changingtheBdown = (e) => {
-    this.state.index >= down ? this.setState({...this.state}) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index  + 3 })
+   changingtheBdown = (e) => {
+    this.state.index >= down ? this.setState({ ...this.state }) : this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index + 3 })
+    if (e.target.id === "down" && this.state.index >= down) {
+      this.setState({ ...this.state, click: 2 })
+    }
+    else{
+      this.setState({ ...this.state, steps: this.state.steps + 1, index: this.state.index + 3 , click: 4})
+    }
   }
 
 
-  reset = (e) => {
-    this.setState({ ...this.state, steps: initialSteps, index: initialIndex })
-  }
+    reset = (e) => {
+      this.setState({ ...this.state, steps: initialSteps, index: initialIndex, click : 4 })
+    }
 
 
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+    // // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
+    // // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
+    // getXY = () => {
+    //   // It it not necessary to have a state to track the coordinates.
+    //   // It's enough to know what index the "B" is at, to be able to calculate them.
+    // }
 
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
-  }
+    // getXYMessage = () => {
+    //   // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
+    //   // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
+    //   // returns the fully constructed string.
+    // }
 
-  reset = () => {
-    // Use this helper to reset all states to their initial values.
-  }
+    // // reset = () => {
+    // //   // Use this helper to reset all states to their initial values.
+    // // }
 
-  getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
+    // getNextIndex = (direction) => {
+    //   // This helper takes a direction ("left", "up", etc) and calculates what the next index
+    //   // of the "B" would be. If the move is impossible because we are at the edge of the grid,
+    //   // this helper should return the current index unchanged.
+    // }
 
-  move = (evt) => {
+    // move = (evt) => {
 
-    // this.useState({...this.state, index : initialIndex  })
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-  }
+    //   // this.useState({...this.state, index : initialIndex  })
+    //   // This event handler can use the helper above to obtain a new index for the "B",
+    //   // and change any states accordingly.
+    // }
 
-  onChange = (evt) => {
-    // You will need this to update the value of the input.
-  }
+    // onChange = (evt) => {
+    //   // You will need this to update the value of the input.
+    // }
 
-  onSubmit = (evt) => {
-    // axios.post(URL, {})
-    // Use a POST request to send a payload to the server.
-  }
+    // onSubmit = (evt) => {
+    //   // axios.post(URL, {})
+    //   // Use a POST request to send a payload to the server.
+    // }
 
-  render() {
-    const { className } = this.props
-    return (
-      <div id="wrapper" className={className}>
-        <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You Moved {this.state.steps} Times</h3>
-        </div>
-        <div id="grid">
-          {/* {
+  
+    render() {
+      const { className } = this.props
+      return (
+        <div id="wrapper" className={className}>
+          <div className="info">
+            <h3 id="coordinates">Coordinates {coordinates[this.state.index]}</h3>
+            <h3 id="steps">You Moved {this.state.steps} Times</h3>
+          </div>
+          <div id="grid">
+            {/* {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
               <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
                 {idx === 4 ? 'B' : null}
               </div>
             ))
           } */}
-          {
-            [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
-                {idx === this.state.index ? 'B' : null}
-              </div>
-            ))
-          }
+            {
+              [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
+                <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
+                  {idx === this.state.index ? 'B' : null}
+                </div>
+              ))
+            }
+          </div>
+          <div className="info">
+            <h3 id="message">{themessage[this.state.click]}</h3>
+          </div>
+          <div id="keypad">
+            <button onClick={this.changingtheBLeft} id="left">LEFT</button>
+            <button onClick={this.changingtheBup} id="up">UP</button>
+            <button onClick={this.changingtheBright} id="right">RIGHT</button>
+            <button onClick={this.changingtheBdown} id="down">DOWN</button>
+            <button onClick={this.reset} id="reset">reset</button>
+          </div>
+          <form >
+            <input id="email" type="email" placeholder="type email"></input>
+            <input id="submit" type="submit"></input>
+          </form>
         </div>
-        <div className="info">
-          <h3 id="message"></h3>
-        </div>
-        <div id="keypad">
-          <button onClick={this.changingtheBLeft} id="left">LEFT</button>
-          <button onClick={this.changingtheBup} id="up">UP</button>
-          <button onClick={this.changingtheBright} id="right">RIGHT</button>
-          <button onClick={this.changingtheBdown}id="down">DOWN</button>
-          <button onClick={this.reset} id="reset">reset</button>
-        </div>
-        <form >
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
-        </form>
-      </div>
     )
   }
 }
